37dc6c75d3560262836dd4501c52a7e446bcca2f
Margulan <thecigaroman@gmail.com>
2020-04-23 20:50:26 +0600

    Init

--- app/View/Components/TestSubject.php
+++ app/View/Components/TestSubject.php
@@ -6,11 +6,13 @@ class TestSubject extends Base
 {
     public function __construct()
     {
+        dump('dumping in constructor');
         dump($this->attributes); // null
     }
 
     public function render()
     {
+        dump('dumping in render');
         dump($this->attributes); // still null
 
         return $this->view('components.test-subject');
